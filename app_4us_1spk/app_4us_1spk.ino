e769714ae2d2075f43975fab599b6d756f2ac5a5
lucasventurar <lucas.ventura.r@gmail.com>
2017-08-16 14:03:02 +0200

    4 ultrasounds 1 speaker tested and working.

--- app_4us_1spk/app_4us_1spk.ino
+++ app_4us_1spk/app_4us_1spk.ino
@@ -4,6 +4,7 @@
 #include <SPI.h>
 #include <SD.h>
 
+
 //TMRpcm library (SD wav card to analog)
 #include "TMRpcm.h"
 
@@ -14,16 +15,26 @@
 int SERIAL_BAUD_RATE = 9600; //debugging
 
 //pin assignement
-int PIN_SD_CS = 53; // Pin 53 at Mega board
-int PIN_SPEAKER = 11; // Pin to audio amplifier input. 5,6,11,46 on Mega
-int PIN_TRIGGER = 2; //ultrasound trigger
-int PIN_ECHO = 3; //ultrasound echo
+int PIN_SD_CS = 53;     // Pin 53 at Mega board
+int PIN_SPEAKER = 11;   // Pin to audio amplifier input. 5,6,11,46 on Mega
+//pin assignment of ultrasounds
+int PIN_TRIGGER1 = 32;  //first  ultrasound trigger
+int PIN_ECHO1 = 33;     //first  ultrasound echo
+int PIN_TRIGGER2 = 34;  //second ultrasound trigger
+int PIN_ECHO2 = 35;     //second ultrasound echo
+int PIN_TRIGGER3 = 36;  //third  ultrasound trigger
+int PIN_ECHO3 = 37;     //third  ultrasound echo
+int PIN_TRIGGER4 = 38;  //fourth ultrasound trigger
+int PIN_ECHO4 = 39;     //fourth ultrasound echo
 
 //play sound TMRpcm object
 TMRpcm tmrpcm;
 
 //ultrasound object
-UltrasoundHCSR04 hcsr04(PIN_TRIGGER, PIN_ECHO);
+UltrasoundHCSR04 hcsr04_1(PIN_TRIGGER1, PIN_ECHO1);
+UltrasoundHCSR04 hcsr04_2(PIN_TRIGGER2, PIN_ECHO2);
+UltrasoundHCSR04 hcsr04_3(PIN_TRIGGER3, PIN_ECHO3);
+UltrasoundHCSR04 hcsr04_4(PIN_TRIGGER4, PIN_ECHO4);
 
 //setup
 void setup()
@@ -52,36 +63,56 @@ void setup()
 void loop()
 {
 
-	double range;
+	double range_1, range_2, range_3, range_4;
 
-	//play a tone
-	// tone(PIN_SPEAKER, 440);
-	// delay(500);
-	// noTone(PIN_SPEAKER);
+  if ( !tmrpcm.isPlaying() )
+  {
+    //get range from ultrasoun
+    range_1 = hcsr04_1.getRange();
+    range_2 = hcsr04_2.getRange();
+    range_3 = hcsr04_3.getRange();
+    range_4 = hcsr04_4.getRange();
+  
+    //print range
+    Serial.print("Range_1: ");
+    Serial.println(range_1, DEC);
+    Serial.print("Range_2: ");
+    Serial.println(range_2, DEC);
+    Serial.print("Range_3: ");
+    Serial.println(range_3, DEC);
+    Serial.print("Range_4: ");
+    Serial.println(range_4, DEC);
+  
+  
+    if ( (0.1 < range_1) && (range_1 < 0.6) ){
+      //play sound
+      tmrpcm.play("phone.wav");
+          delay(500);
+    }
+  
+    else if ( (0.1 < range_2) && (range_2 < 0.6) ){
+      //play sound
+      tmrpcm.play("alarm.wav");
+          delay(500);
+    }
+  
+    else if ( (0.1 < range_3) && (range_3 < 0.6) ){
+      //play sound
+      tmrpcm.play("alarm.wav");
+          delay(500);
+    }
+  
+    else if ( (0.1 < range_4) && (range_4 < 0.6) ){
+      //play sound
+      tmrpcm.play("phone.wav");
+          delay(500);
+    }
+  }
 
-	//check if already playing a sound
-	if ( !tmrpcm.isPlaying() )
-	{
-		//get range from ultrasound
-		range = hcsr04.getRange();
-		Serial.print("range: ");
-		Serial.println(range, DEC);
-		if ( (0.1 < range) && (range < 0.3) )
-		{
-			//play sound
-			tmrpcm.play("phone.wav");
-			//delay(500);
-		}
-
-		if ( (0.3 < range) && (range < 0.6) )
-		{
-			//play sound
-			tmrpcm.play("alarm.wav");
-			//delay(500);
-		}
-	}
-	else //if playing, just wait a little bit
-	{
-		delay(500);
-	}
+  else{
+    tmrpcm.disable();
+    delay(100);
+   // noTone(PIN_SPEAKER);
+    
+  }
 }
